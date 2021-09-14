4ef7926287e4e9a0512dc74c8f26493b01342fe5
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-09-14 22:28:15 +0300

    finished topBar

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,12 @@
+import { Container } from "../../general";
+import "./intro.scss";
+
+function Intro() {
+  return (
+    <section className="intro" id="intro">
+      <Container className="intro__content"></Container>
+    </section>
+  );
+}
+
+export default Intro;
