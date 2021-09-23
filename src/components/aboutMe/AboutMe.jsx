caaa02d87344b05d16645ffea8f3d81b817a96b0
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2021-09-23 23:38:18 +0300

    finished @media

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -6,21 +6,23 @@ import "./aboutMe.scss";
 function AboutMe() {
   return (
     <section className="aboutMe" id="aboutMe">
-      <Container className="aboutMe__wrapper">
-        <div className="aboutMe__image"></div>
-        <div className="aboutMe__content">
-          <h2>about me</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            repellat eos labore, totam expedita voluptatibus, debitis architecto
-            vel repellendus porro nostrum ad tempore, impedit fugit autem saepe
-            ea incidunt laborum.
-          </p>
+      <Container>
+        <h2>about me</h2>
+        <div className="aboutMe__wrapper">
+          <div className="aboutMe__image"></div>
+          <div className="aboutMe__content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique repellat eos labore, totam expedita voluptatibus,
+              debitis architecto vel repellendus porro nostrum ad tempore,
+              impedit fugit autem saepe ea incidunt laborum.
+            </p>
 
-          <a href="#skills">
-            View my skills
-            <ArrowForwardIcon className="icon" />
-          </a>
+            <a href="#skills">
+              View my skills
+              <ArrowForwardIcon className="icon" />
+            </a>
+          </div>
         </div>
       </Container>
     </section>
